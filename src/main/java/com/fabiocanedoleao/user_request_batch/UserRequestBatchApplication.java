e90f6262cfbebaa90e1e1ccfa7ae037e5a00ed36
FÁBIO CANEDO LEÃO <236462@facens.br>
2024-10-30 08:52:57 -0300

    Project created and Job Config

---
+++ src/main/java/com/fabiocanedoleao/user_request_batch/UserRequestBatchApplication.java
@@ -0,0 +1,13 @@
+package com.fabiocanedoleao.user_request_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRequestBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRequestBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabiocanedoleao/user_request_batch/UserRequestBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabiocanedoleao.user_request_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRequestBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
